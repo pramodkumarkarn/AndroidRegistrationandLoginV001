b3d250517799c94b405846af10a0f27b087e08c7
KangSDhi <kangteknisi@gmail.com>
2018-09-04 16:01:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidregistrationandlogin/LoginActivity.java
@@ -0,0 +1,137 @@
+package com.example.androidregistrationandlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button btn_login;
+    private TextView link_regist;
+    private ProgressBar loading;
+    private static String URL_LOGIN = "http://192.168.1.4/android_register_login/login.php";
+    SessionManager sessionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        sessionManager = new SessionManager(this);
+
+        loading = findViewById(R.id.loading);
+        email = (EditText) findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        link_regist = findViewById(R.id.link_regist);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = email.getText().toString().trim();
+                String mPassword = password.getText().toString().trim();
+
+                if (!mEmail.isEmpty() || !mPassword.isEmpty()){
+                    Login(mEmail, mPassword);
+                }else {
+                    email.setError("Please Insert email!");
+                    password.setError("Please Insert Pasword!");
+                }
+            }
+        });
+
+        link_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void Login(final String mEmail, final String mPassword) {
+        loading.setVisibility(View.VISIBLE);
+        btn_login.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                            if (success.equals("1")){
+
+                                for (int i = 0; i < jsonArray.length(); i++){
+
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String name = object.getString("name").trim();
+                                    String email = object.getString("email").trim();
+
+                                    sessionManager.createSession(name, email);
+
+                                   Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                   intent.putExtra("name", name);
+                                   intent.putExtra("email", email);
+                                   startActivity(intent);
+
+                                    loading.setVisibility(View.GONE);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loading.setVisibility(View.GONE);
+                            btn_login.setVisibility(View.VISIBLE);
+                            Toast.makeText(LoginActivity.this, "Error : "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.setVisibility(View.GONE);
+                        btn_login.setVisibility(View.VISIBLE);
+                        Toast.makeText(LoginActivity.this, "Error "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", mEmail);
+                params.put("password", mPassword);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+}
